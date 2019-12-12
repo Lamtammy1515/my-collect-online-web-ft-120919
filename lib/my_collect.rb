4eda98a064d69938a1c88ce3b5efdf27055d0fe4
Tammy Lam <lamtammy1515@gmail.com>
2019-12-12 21:49:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
 
+require 'pry'
 
+def my_collect(array)
+  index = 0
+  new_array = []
+ 
+ 
+ 
+  while index < array.length
+    new_array << yield(array[index])
+    index += 1
+  end
+  
+  
+  new_array
+end
+
+languages = ['ruby, javascript', 'python', 'objective-c']
+
+my_collect(languages) do |language|
+  language.upcase
+  
+end
+
+#binding.pry
--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -1,6 +1,9 @@
 describe "my_collect" do
-  let(:languages) { ['ruby', 'javascript', 'python', 'objective-c'] }
+  let(:languages) do { ['ruby', 'javascript', 'python', 'objective-c'] }
+ 
+
   let(:students) { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
+
 
   it "can handle an empty collection" do
     empty_array = []
